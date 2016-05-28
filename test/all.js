13bf184de1a2ef65bbf13422e37ebe3c5d9b9fbe
Joao Andrade <jjadonotenter@gmail.com>
2016-05-28 16:37:24 +0100

    Restructure tests so changes occur in the correct order

--- test/all.js
+++ test/all.js
@@ -4,44 +4,44 @@ var expect  = require('expect.js'),
     fs      = require('fs')
 
 Browser.localhost('localhost', 1337)
-var browser = Browser.create()
 
-describe('livedown', function(){
+describe('livedown', function(done){
   before(function(){
     server.start('test/fixtures/basic.md')
   })
 
-  it('renders markdown correctly', function(done){
-    browser.visit('/', function(error) {
-      if (error) { return done(error) }
+  before(function(done){
+    this.browser = Browser.create()
+    this.browser.visit('/', done)
+  })
 
-      expect(browser.evaluate("$('.markdown-body h1').text()")).to.be('h1')
-      done()
-    });
+  it('renders markdown correctly', function(done){
+    expect(this.browser.window.$(".markdown-body h1").text()).to.be('h1')
+    done()
   })
 
   describe('content changes', function(){
     var fixtureContent,
         fixturePath = 'test/fixtures/basic.md'
 
-    beforeEach(function(){
+    before(function(){
       fixtureContent = fs.readFileSync(fixturePath, 'utf8')
     })
 
-    it('live updates the rendered markdown', function(done){
-      fs.writeFile(fixturePath, '## h2', function(error){
-        if (error) { return done(error) }
-
-        browser.visit('/', function(error) {
-          if (error) { return done(error) }
-
-          expect(browser.evaluate("$('.markdown-body h2').text()")).to.be('h2')
-          done()
-        })
-      })
+    before(function(done) {
+      fs.writeFile(fixturePath, '## h2', done)
     })
 
-    afterEach(function(){
+    before(function(done) {
+      this.browser.visit('/', done)
+    })
+
+    it('live updates the rendered markdown', function(done){
+      expect(this.browser.window.$(".markdown-body h2").text()).to.be('h2')
+      done()
+    })
+
+    after(function(){
       fs.writeFileSync(fixturePath, fixtureContent)
     })
   })
