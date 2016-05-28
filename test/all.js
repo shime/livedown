2b0f34b29ef08b93c4792230361fe8a8c9711912
Joao Andrade <jjadonotenter@gmail.com>
2016-05-28 04:08:48 +0100

    Revert "Upgrade to Zombie.js 4"
    This reverts commit e610cba40b8fa58551490142f49b5acb358fdcaa.

--- test/all.js
+++ test/all.js
@@ -5,7 +5,7 @@ var expect  = require('expect.js'),
     fs      = require('fs')
 
 Browser.localhost('localhost', 1337)
-var browser = new Browser()
+var browser = Browser.create()
 
 describe('livedown', function(){
   before(function(){
