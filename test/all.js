30416b068207d1b5047b118347355331f683dcda
Osman Koçak <kocakosm@gmail.com>
2017-08-09 00:12:33 +0200

    --browser now accepts options to be passed to the browser executable

--- test/all.js
+++ test/all.js
@@ -4,8 +4,28 @@ var expect = require('expect.js')
 var Browser = require('zombie')
 var server = require('./../server')()
 var fs = require('fs')
+var utils = require('./../utils')
 
 var browser = new Browser()
+
+describe('utils.splitCommandLine', function () {
+  it('returns an empty array when given a falsy input', function () {
+    expect(utils.splitCommandLine(undefined)).to.be.an(Array)
+    expect(utils.splitCommandLine(undefined)).to.be.empty()
+    expect(utils.splitCommandLine(null)).to.be.an(Array)
+    expect(utils.splitCommandLine(null)).to.be.empty()
+  })
+
+  it('splits the input string by spaces', function () {
+    expect(utils.splitCommandLine('firefox -new-window')).to.be.an(Array)
+    expect(utils.splitCommandLine('firefox -new-window')).to.eql(['firefox', '-new-window'])
+  })
+
+  it('ignores spaces in quoted parts', function () {
+    expect(utils.splitCommandLine("'google chrome' --incognito")).to.be.an(Array)
+    expect(utils.splitCommandLine("'google chrome' --incognito")).to.eql(['google chrome', '--incognito'])
+  })
+})
 
 describe('livedown', function () {
   before(function (done) {
--- utils.js
+++ utils.js
@@ -3,5 +3,12 @@ var path = require('path')
 module.exports = {
   isPathRelative: function (p) {
     return path.normalize(p) !== path.resolve(p)
+  },
+  splitCommandLine: function (cmd) {
+    if (cmd) {
+      return cmd.match(/(?:[^\s"']+|["'][^"']*["'])+/g)
+                .map(function (s) { return s.replace(/["']/g, '') })
+    }
+    return []
   }
 }
