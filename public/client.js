5039ad4bb4f230ed76986da1975239ea6dc3e5a2
Hunter Gatewood <hgatewood@gmail.com>
2019-03-09 00:52:21 -0500

    Add full functionality when following Markdown links

--- public/client.js
+++ public/client.js
@@ -4,6 +4,36 @@ var socket = io.connect(window.location.origin)
 
 hljs.configure({languages: []})
 
+pageshow = function (event) {
+  // HACK: you would assume we wouldn't ever need to reload
+  // the window, and instead could just send 'ready' again;
+  // I don't know why this doesn't work :(
+  if (event.persisted)
+    window.location.reload()
+  else
+    socket.emit('hello', window.location.pathname)
+}
+
+pagehide = function (event) {
+  $('body').css('opacity', '0')
+  socket.emit('goodbye', window.location.pathname)
+}
+
+// HACK: override the page cache. To do this, we need
+// to always reload the page (on pageshow). However, this
+// will show the previous/cached version of the page for a split
+// second, so to work around this we make the body's opacity 0 whenever
+// leaving a page (on pageleave).
+// Inspiration:
+//    - https://webkit.org/blog/516/webkit-page-cache-ii-the-unload-event/
+//    - https://github.com/yujiosaka/socket.io-cache-example
+//    - https://developer.mozilla.org/en-US/docs/Web/Events/beforeunload
+// Related:
+//    - https://webkit.org/blog/427/webkit-page-cache-i-the-basics/
+//    - https://developer.mozilla.org/en-US/docs/Mozilla/Firefox/Releases/1.5/Using_Firefox_1.5_caching#New_browser_events
+window.addEventListener('pageshow', pageshow)
+window.addEventListener('beforeunload', pagehide)
+
 socket.on('content', function (data) {
   $('.markdown-body').html(data)
   $('code').each(function (_, block) {
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var markdownItTaskCheckbox = require('markdown-it-task-checkbox')
 var markdownItEmoji = require('markdown-it-emoji')
 var markdownItGitHubHeadings = require('markdown-it-github-headings')
 // Note: using a forked version of markdown-it-katex until
-// the official version updates to comply with katex 0.10.x
+// the official version updates to support katex 0.10.x
 var markdownItKatex = require('@iktakahiro/markdown-it-katex')
 
 function getMd () {
@@ -28,14 +28,11 @@ function getMd () {
   md.use(markdownItKatex)
   return md
 }
-
 var md = getMd()
 
 var app = express()
 var server = http.Server(app)
 var io = socket(server)
-
-var utils = require('./utils')
 
 module.exports = function (opts) {
   return new Server(opts)
@@ -48,22 +45,35 @@ function Server (opts) {
 
   this.port = opts.port || 1337
   this.URI = 'http://localhost:' + this.port
-  this.sock = {emit: function () {}}
+  this.pathToSock = {}
+  this.dirname = ''
+  this.relativePath = ''
+  this.originalRelativePath = ''
+  this.watcher = chokidar.watch()
+
+  this.watcher.on('change', function (changedPath, stats) {
+    var relativePath = path.join(
+        '/',  // relative to pseudo-root of web server
+        path.relative(self.dirname, changedPath))
+
+    // early return for performance
+    if (!(relativePath in self.pathToSock))
+      return
+
+    md = getMd()
+    fs.readFile(changedPath, 'utf8', function (err, data) {
+      if (err) throw err
+      data = data || ''
+
+      // early return for correctness
+      if (!(relativePath in self.pathToSock))
+        return
+      self.pathToSock[relativePath].emit('content', md.render(data))
+    })
+  })
 
   this.listen = function (next) {
     server.listen(self.port, next)
-  }
-
-  this.watch = function (path) {
-    var self = this
-    chokidar.watch(path).on('change', function (path, stats) {
-      md = getMd()
-      fs.readFile(path, 'utf8', function (err, data) {
-        if (err) throw err
-        data = data || ''
-        self.sock.emit('content', md.render(data))
-      })
-    })
   }
 }
 
@@ -77,35 +87,71 @@ Server.prototype.stop = function (next) {
 
 Server.prototype.start = function (filePath, next) {
   var self = this
-  var sendFileOpts = {}
 
-  if (utils.isPathRelative(filePath)) {
-    sendFileOpts.root = path.resolve(__dirname)
-  }
+  // set the pseudo-root of the web server to the
+  // dirname of the argv-passed file
+  self.dirname = path.dirname(filePath)
 
+  // current file being served
+  self.relativePath = path.basename(filePath)
+
+  // original file being served
+  self.originalRelativePath = self.relativePath
+
+  // stop any previously-started Livedown server listening on this port
   this.stop(function () {
-    self.watch(filePath)
     self.listen(next)
   })
 
   io.on('connection', function (sock) {
-    self.sock = sock
-    self.sock.emit('title', path.basename(filePath))
-    md = getMd()
-    fs.readFile(filePath, 'utf8', function (err, data) {
-      if (err) throw err
-      data = data || ''
-      self.sock.emit('content', md.render(data))
+
+    // HACK: dealing with the back/forward cache; see client.js
+
+    // Markdown file asking for rendered content
+    sock.on('hello', function (relativePath) {
+      var fullPath = path.join(self.dirname, relativePath)
+      self.pathToSock[relativePath] = sock
+      self.watcher.add(fullPath)
+      md = getMd()
+      fs.readFile(fullPath, 'utf8', function (err, data) {
+        if (err) throw err
+        data = data || ''
+        var s = self.pathToSock[relativePath]
+        s.emit('title', path.basename(self.relativePath))
+        s.emit('content', md.render(data))
+      })
+    })
+
+    // Markdown file indicating it doesn't currently need attention
+    sock.on('goodbye', function(relativePath) {
+      delete self.pathToSock[relativePath]
+      self.watcher.unwatch(path.join(self.dirname, relativePath))
     })
   })
 
   app.use(parser.json())
-  app.get('/', function (req, res) {
+  app.use(express.static(path.join(__dirname, 'public')))
+
+  // redirect the root path to the file specified
+  // in command line args
+  app.get('/', function(req, res) {
+    res.redirect(self.originalRelativePath)
+  })
+
+  // handle all Markdown files appropriately:
+  //    - send index.html
+  //    - the Markdown file itself will later be rendered
+  //      into HTML and sent via socket.io
+  app.get('/*.md', function (req, res) {
+    self.relativePath = req.path
     res.sendFile(path.join(__dirname, 'index.html'))
   })
-  app.use(express.static(path.join(__dirname, 'public')))
+
+  // serve non-Markdown static files as-is
   app.use(express.static(path.dirname(filePath)))
 
+  // provides functionality for killing the process via HTTP,
+  // e.g. for the editor plugins
   app.delete('/', function (req, res) {
     io.emit('kill')
     res.end()
