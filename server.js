e98cb394f01c8b56cb87f2270c9ccb6ef1309328
Mark Furland <mtfurlan@i3detroit.org>
2022-03-21 10:45:45 -0400

    Add markdown-it-footnote
    Match github footnote support

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var markdownIt = require('markdown-it')
 var markdownItTaskCheckbox = require('markdown-it-task-checkbox')
 var markdownItEmoji = require('markdown-it-emoji')
 var markdownItGitHubHeadings = require('markdown-it-github-headings')
+var markdownItFootnote = require('markdown-it-footnote')
 
 var md = markdownIt({
   html: true,
@@ -21,6 +22,7 @@ md.use(markdownItEmoji)
 md.use(markdownItGitHubHeadings, {
   prefix: ''
 })
+md.use(markdownItFootnote)
 
 var app = express()
 var server = http.Server(app)
